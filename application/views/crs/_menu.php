68fa74b1f68f0d835b815c06f5dcb16bcc3e9d64
radhiva951753 <radhivah@gmail.com>
2023-11-30 17:13:06 +0700

    first commit

---
+++ application/views/crs/_menu.php
@@ -0,0 +1,22 @@
+<div class="col-md-12" style="background:url(assets/global/img/background.jpg); opacity:0.7; height:00px;">
+    <div class="note note-bordered note-danger" style="text-align:center">
+        <h4>Ini adalah aplikasi yang akan membantu anda mendapatkan mobil berdasarkan kebutuhan anda. Pencarian mobil dilakukan menggunakan conversational recommender system berdasarkan kebutuhan fungsional yang anda inginkan</h4>
+        <br>
+        <div class="tiles">
+        	<div class="tile double" style="cursor:default">
+            </div>
+            <div class="tile" style="cursor:default">
+            </div>
+        	<div class="tile double  bg-blue"><a href="javascript:u1()">
+                <div class="tile-body">
+                    <i class="fa fa-car"></i>
+                </div>
+                <div class="tile-object">
+                    <div class="name">
+                         Click Untuk Melakukan Rekomendasi
+                    </div>
+                </div>
+            </div>
+        </div>      
+    </div>
+</div>
